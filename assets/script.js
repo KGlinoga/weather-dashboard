00e93533602952bfcd49424a9ab6f93f9a1dc45b
Kristina Faye <kglinoga@gmail.com>
2022-07-06 17:32:05 -0700

    entered pseudo code in HTML, CSS and JS.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
 console.log("hello");
+
+// input form for users to search for a city
+    // saving searches to local storage
+    // appending searches in local storage to individual buttons
+    //event listener for button clicks that search the app for that city
+
+// Main card:
+    //h2 that updates with CURRENT CITY AND CURRENT DATE
+    // p tags that update with that city's current Temp, Wind, Humidity, and UVI
+        // p tag that displays the UVI value's background color updates depending on the UVI severity (if statements) 
+        
+// Section:
+    //5 cards that fill in each day's forecast in abovementioned City, starting the day AFTER Current Day
+        //dynamically updated forecast includes: 
+            // Date of forecast
+            // weather icon
+            // temp
+            // wind
+            // humidity
+
+            
