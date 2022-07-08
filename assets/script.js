6e31d37f5bcfe8bdbab0947d81e241d4aee9ad08
Kristina Faye <kglinoga@gmail.com>
2022-07-07 21:16:08 -0700

    OMG got the weather data to load AND displaygit add .

--- assets/script.js
+++ assets/script.js
@@ -26,6 +26,47 @@ function newCity() {
 
 // section1 card:
     //h2 that updates with CURRENT CITY AND CURRENT DATE
+    // var tableBody = document.getElementById('repo-table');
+    // var fetchButton = document.getElementById('fetch-button');
+    var currentCity = document.getElementById("#current-city");
+    // *Just remember this exists - 
+    // var searchBtn = document.querySelector("#search-city");
+
+
+    function getApi() {
+      // fetch request gets a list of all the repos for the node.js organization
+      var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=2bd5559124f13def23addea6864b8f2c';
+    
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          // Creating elements, tablerow, tabledata, and anchor - this is more like .textContent b/c we did the HTML already.
+        //     var createTableRow = document.createElement('tr');
+        // var currentDate = moment().format("MM DD, YYYY")
+        var currentTemp = document.querySelector("#current-temp");
+        currentTemp.textContent = "Current Temp: " + data.current.temp;
+        //     var tableData = document.createElement('td');
+        //     var link = document.createElement('a');
+    
+        //     // Setting the text of link and the href of the link
+        //     link.textContent = data[i].html_url;
+        //     link.href = data[i].html_url;
+    
+        //     // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        //     // The tablerow then gets appended to the tablebody
+        //     tableData.appendChild(link);
+        //     createTableRow.appendChild(tableData);
+        //     tableBody.appendChild(createTableRow);
+          });
+    //     });
+    }
+      getApi();
+    // fetchButton.addEventListener('click', getApi);
+
+
     // p tags that update with that city's current Temp, Wind, Humidity, and UVI
         // p tag that displays the UVI value's background color updates depending on the UVI severity (if statements) 
 
